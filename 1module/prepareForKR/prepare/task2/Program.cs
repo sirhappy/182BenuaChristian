9b430fc2a1e08df8a92d9abdb0e66f76e7ec705b
ChrisBenua <kpbenua@edu.hse.ru>
2018-10-16 06:23:54 +0300

    prepare for kr added

---
+++ 1module/prepareForKR/prepare/task2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+class Program
+{
+
+    public static void ReadInt(out int n, string messageIn, string messageOut, Func<int, bool> validate)
+    {
+        Console.WriteLine(messageIn);
+        while (!(int.TryParse(Console.ReadLine(), out n) && validate(n)))
+        {
+            Console.WriteLine(messageOut);
+        }
+    }
+
+    public static void ReadDouble(out double n, string messageIn, string messageOut, Func<double, bool> validate)
+    {
+        Console.WriteLine(messageIn);
+        while (!(double.TryParse(Console.ReadLine(), out n) && validate(n)))
+        {
+            Console.WriteLine(messageOut);
+        }
+    }
+
+    public static double[] CreateArray(int n) {
+        double[] a = new double[n];
+        //Random rnd = new Random();
+        for (int i = 0; i < n; ++i) {
+            a[i] = 1.0 / (2 * i + 1);
+            a[i] *= i % 2 == 0 ? -1 : 1;
+        }
+        /*a[0] = -1;
+        a[1] = 0;
+        a[2] = 5;
+        a[3] = -7;
+        a[4] = 11;*/
+        /*a[0] = 1;
+        a[1] = 2;
+        a[2] = -4;
+        a[3] = 6;
+        a[4] = -8;*/
+        return a;
+    }
+
+    public static void ShowArray(ref double[] arr) {
+        for (int i = 0; i < arr.Length; ++i) {
+            Console.Write(arr[i].ToString("F3") + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public static void ShiftArray(ref double[] arr) {
+        //int pt = 0;
+        for (int i = 0; i < arr.Length; ++i) {
+            if (arr[i] < 0) {
+                arr[i] = i == 0? 0 : arr[0];
+                for (int j = i - 1; j >= 0; --j) {
+                    double t = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = t;
+                }
+            }
+        }
+        //Array.Resize(ref arr, pt);
+    }
+
+
+    static void Main(string[] args)
+    {
+        ConsoleKeyInfo keyInfo;
+        do
+        {
+            int k;
+            ReadInt(out k, "Enter size of an array", "Smth wrong, reenter size of array", (arg) => arg > 0 && arg < 1e6);
+            double[] arr = CreateArray(k);
+            ShowArray(ref arr);
+            ShiftArray(ref arr);
+            ShowArray(ref arr);
+            Console.WriteLine("Press esc to exit");
+            keyInfo = Console.ReadKey();
+        } while (keyInfo.Key != ConsoleKey.Escape);
+    }
+}
+
---
+++ 1module/prepareForKR/prepare/task4/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+
+class Program
+{
+    static int size = 201;
+    public static void ReadInt(out int n, string messageIn, string messageOut, Func<int, bool> validate)
+    {
+        Console.WriteLine(messageIn);
+        while (!(int.TryParse(Console.ReadLine(), out n) && validate(n)))
+        {
+            Console.WriteLine(messageOut);
+        }
+    }
+
+    public static void ReadDouble(out double n, string messageIn, string messageOut, Func<double, bool> validate)
+    {
+        Console.WriteLine(messageIn);
+        while (!(double.TryParse(Console.ReadLine(), out n) && validate(n)))
+        {
+            Console.WriteLine(messageOut);
+        }
+    }
+
+    static double[,] binomialCoef;
+
+    public static void fillBinomial() {
+        binomialCoef = new double[size, size];
+        binomialCoef[0,0] = 1;
+        for (int i = 1; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                int prev = j - 1;
+                if (prev >= 0) {
+                    binomialCoef[i, j] += binomialCoef[i - 1, prev];
+                }
+                binomialCoef[i, j] += binomialCoef[i - 1, j];
+            }
+        }
+    }
+
+    public static double[] CreateArray(int n) {
+        double[] arr = new double[n];
+        arr[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                arr[i] -= binomialCoef[i + 1, j + 1] * arr[j];
+            }
+            arr[i] *= 1.0 / (i + 1);
+        }
+        return arr;
+    }
+
+    public static void SummCalculate(double[] arr, int pref, out double s1, out double s2) {
+        s1 = s2 = 0;
+        for (int i = 0; i < pref; i += 2) {
+            s1 += arr[i];
+        }
+        for (int i = 1; i < pref; i += 2) {
+            s2 += arr[i];
+        }
+    }
+
+    public static void Print(int a, double b, double c) {
+        Console.WriteLine("{0}: S1={1}, S2={2}", a, b.ToString("F3"), c.ToString("F3"));
+    }
+
+    static void Main(string[] args)
+    {
+        fillBinomial();
+        //CreateArray(20);
+        ConsoleKeyInfo keyInfo;
+        do
+        {
+            int n;
+            ReadInt(out n, "Enter size of sequence", "Smth wrong with your size of sequence, reenter pls", (arg) => arg < 200 && arg > 0);
+            double[] arr = CreateArray(n);
+            double s1, s2;
+            double mx = double.MinValue;
+            int ansInd = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                s1 = s2 = 0;
+                SummCalculate(arr, i, out s1, out s2);
+                if (mx <  Math.Abs(s1 + s2)) {
+                    mx = Math.Abs(s1 + s2);
+                    ansInd = i;
+                }
+                Print(i, s1, s2);
+
+            }
+            Console.WriteLine("maximum abs value at: " + ansInd);
+            Console.WriteLine("Press esc to exit");
+            keyInfo = Console.ReadKey();
+        } while (keyInfo.Key != ConsoleKey.Escape);
+    }
+}
+
